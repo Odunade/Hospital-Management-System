44cae3c9e4ad3a47f6b1fa9428e9935c05958183
ODUNADE AHMED <88735479+Odunade@users.noreply.github.com>
2022-09-18 00:26:19 +0100

    Header.js created

---
+++ Nollywood Movies & Tv Series App/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div>
+    <span className='header'>🎬Hollywood Movies & TV Series🎥</span>
+
+    </div>
+  )
+}
+
+export default Header
