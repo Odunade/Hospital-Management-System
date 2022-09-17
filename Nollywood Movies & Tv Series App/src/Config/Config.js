9f8e23e11e3e1de80a01de6c2e12e4d782366496
ODUNADE AHMED <88735479+Odunade@users.noreply.github.com>
2022-09-18 00:33:51 +0100

    Config.js commit

---
+++ Nollywood Movies & Tv Series App/src/Config/Config.js
@@ -0,0 +1,15 @@
+// image sizes for tmdb
+export const img_300 = "https://image.tmdb.org/t/p/w300";
+export const img_500 = "https://image.tmdb.org/t/p/w500";
+
+// contentModal and SingleContent
+export const unavailable =
+"https://www.movienewz.com/img/films/poster-holder.jpg";
+
+// contentModal
+export const unavailableLandscape =
+"https://user-images.githubusercontent.com/10515204/56117400-9a911800.jpg";
+
+// For carousel
+export const noPicture =
+"https://upload.wikimedia.org/wikipedia/en/6/60/No_Picture.jpg";
